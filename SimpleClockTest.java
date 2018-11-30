eef25e975ee37dcd40434a9f440822712f2bc868
BBaughn100 <45470825+BBaughn100@users.noreply.github.com>
2018-11-29 19:18:12 -0500

    Update SimpleClockTest.java

--- SimpleClockTest.java
+++ SimpleClockTest.java
@@ -1,15 +1,4 @@
-/*
- *   A program that tests the SimpleClock class.  This program instantiates a
- *   new SimpleClock object, sets it to 11:59:00PM and then repeatedly ticks
- *   seconds to show the passage of an hours worth of time.  YOU DO NOT NEED
- *   TO MODIFY THIS CLASS.  Make your modifications to the SimpleClock.java
- *   file instead.
- *
- *
- * @author Jeremy Morris
- * @version 20161212
- *
- */
+
 public class SimpleClockTest {
 
     public static void main(String[] args) {
@@ -30,11 +19,11 @@ public class SimpleClockTest {
         clock.tick();
         System.out.println("TICK: " + clock);
         System.out.println("Clock should read: 12:00:00PM");
-        clock.set(12, 59, 59, false);
+        clock.set(5, 35, 59, false);
         System.out.println("Clock has been set to time: " + clock);
         clock.tick();
         System.out.println("TICK: " + clock);
-        System.out.println("Clock should read: 1:00:00PM");
+        System.out.println("Clock should read: 5:36:00PM");
         System.out.println("Clock finally reads: " + clock);
     }
 
